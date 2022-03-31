541ba14ac8259c3e8efc9f5e88e894e4ad25af5c
Amy Zhao <azhao@adobe.net>
2022-03-31 14:59:51 -0500

    Made changes based on feedback

--- src/validate-removal-author.js
+++ src/validate-removal-author.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 
 const myArgs = process.argv.slice(2);
 const creator = myArgs[0];
-const npm_link = myArgs[1]
+const npmLink = myArgs[1]
 
 fetch('https://api.github.com/repos/adobe/aio-template-submission/issues?state=closed&labels=add-template&creator=' + creator)
   .then(response => response.json())
@@ -12,7 +12,7 @@ fetch('https://api.github.com/repos/adobe/aio-template-submission/issues?state=c
     if(data.length == 0) {
       throw new Error('No add-template issues submitted by creator found.')
     } else {
-      let found = data.find(element => element.body.includes(npm_link))
+      let found = data.find(element => element.body.includes(npmLink))
       if(found == undefined) {
         throw new Error('Matching add-template issue for removal request by creator not found.')
       } else {
